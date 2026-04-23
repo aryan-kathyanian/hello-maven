2304280f1b630fadf6dfe8aeca44b813ef7dfbdf
Aryan Kathyanian <aryanpathania779@gmail.com>
2026-04-23 13:57:28 +0530

    Added maven project structure

---
+++ src/main/java/com/aryan/kathyanian/App.java
@@ -0,0 +1,13 @@
+package com.aryan.kathyanian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
